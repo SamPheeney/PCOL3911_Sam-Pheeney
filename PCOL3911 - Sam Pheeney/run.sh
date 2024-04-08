ee27699c86d274e9d79c699fa7b49f61cd65f16e
Sam Pheeney <165231001+SamPheeney@users.noreply.github.com>
2024-04-08 11:30:41 +1000

    Delete PCOL3911 - Sam Pheeney/run.sh

--- PCOL3911 - Sam Pheeney/run.sh
+++
@@ -1,6 +0,0 @@
-# this line installs the necessary libraries
-# it gets the list of libraries from the file requirements.txt
-python3 -m pip install -r requirements.txt
-#this line runs the python file
-python3 generate_fp_from_smiles.py
-data = pd.read_csv("gsar_a_1049665_sm4518.csv")
